dde6ef0bf5c6d47c44f6059380f137c089ba9c58
shadid12 <Administrator@mtest2.dev.ibm-cloud.com>
2016-06-23 03:42:05 -0500

    stuff

---
+++ Logic.R
@@ -0,0 +1,28 @@
+swirl() # include library 
+
+(FALSE == TRUE) == FALSE
+# [1] TRUE
+#  The equality operator can also be used to compare numbers. Use `==` to see if 6 is equal to 7.
+6 == 7
+# False
+# Write an expression to test whether 6 is less than 7
+6 < 7
+10 <=10 
+# unequality check 
+5 != 7
+# is 5 equal to 7
+!5 == 7 
+# ambiguious case 
+5 > 8 || 6 != 8 && 4 > 3.9
+xor(5 == 6,!FALSE)
+# Use the which() function to find the indices of ints that are greater than 7.
+which(ints > 7)
+# [1]  3  4 10
+any(ints < 0)
+# Use the all() function to see if all of the elements of ints are greater than zero.
+all(ints > 0)
+
+
+
+
+
