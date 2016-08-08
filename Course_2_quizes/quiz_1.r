635ce05c68714d4ddd8ce7dde019c9896a7ff1f6
shadid12 <shadidhaque2014@gmail.com>
2016-08-08 12:11:32 -0400

    asdasd

--- Course_2_quizes/quiz_1.r
+++ Course_2_quizes/quiz_1.r
@@ -1,1 +1,14 @@
+dir.create("data")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+download.file(fileUrl, destfile = "./data/06hid.csv", method = "curl")
+dateDownloaded <- date()
 
+
+# install necessary libraries 
+library(dplyr)
+packageVersion("dplyr")
+# # #  # # ########### 
+
+h <- read.csv("./data/06hid.csv")
+df <- tbl_df(h)
+sum <- filter(df, VAL >= 24)
