8dad4443f6a2a7e53f8f3b73f6237180e26a1d92
shadid12 <shadidhaque2012@gmail.com>
2016-08-09 23:28:26 -0400

    my sql

---
+++ course_2_week_2.R
@@ -0,0 +1,17 @@
+### My SQL ####
+
+install.packages("RMySQL")
+library("RMySQL")
+
+uscDB <- dbConnect(MySQL(), user="genome", host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(uscDB, "show databases;")
+dbDisconnect(uscDB)
+
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19",
+                  host="genome-mysql.cse.ucsc.edu")
+alltables <- dbListTables(hg19)
+alltables[1:5]
+dbListFields(hg19, "HInv")
+dbDisconnect(hg19)
+
+
