b15215aa9b594e6270b4d5210f5a3039cf1a5de3
shadid12 <shadidhaque2012@gmail.com>
2016-08-09 23:32:12 -0400

    commit all changes

--- Course_2_quizes/quiz_1.r
+++ Course_2_quizes/quiz_1.r
@@ -1,1 +1,19 @@
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv",destfile = "C:/Users/shadid/Desktop/r/data.csv" )
 
+dat = read.csv("C:/Users/shadid/Desktop/r/data.csv", header = TRUE)
+library(dplyr)
+library(readxl)
+
+library(xlsx)
+
+mydat <- tbl_df(dat)
+over_mil <- filter(mydat, VAL == 24)
+# 53 
+
+# Question 2: tidy data has one variable per coloum
+
+## Question 3:
+
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx", destfile = "C:/Users/shadid/Desktop/r/gas.xlsx")
+dateDownloaded <- date()
+new_dat <- read.xlsx()
--- course_2_week_2.R
+++ course_2_week_2.R
@@ -11,6 +11,7 @@ hg19 <- dbConnect(MySQL(), user="genome", db="hg19",
                   host="genome-mysql.cse.ucsc.edu")
 alltables <- dbListTables(hg19)
 alltables[1:5]
+
 dbListFields(hg19, "HInv")
 dbDisconnect(hg19)
 
